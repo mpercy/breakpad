37701b490f1a539d3ca559ece7576e2f2dfd6e36
Mike Percy <mpercy@cloudera.com>
2016-04-29 11:37:58 -0700

    improvements to archive maker

--- mk_archive.sh
+++ mk_archive.sh
@@ -3,7 +3,14 @@
 
 VERSION=snapshot
 STAGING=staging
+rm -rf "$STAGING"
 git archive --format=tar --prefix=breakpad-$VERSION/ HEAD | (mkdir -p "$STAGING" && cd "$STAGING" && tar xf -)
-cd "$STAGING"
+pushd "$STAGING"
 git clone https://chromium.googlesource.com/linux-syscall-support breakpad-$VERSION/src/google_breakpad/third_party/lss
-tar czvf breakpad-$VERSION.tar.gz breakpad-$VERSION/
+FILE="breakpad-$VERSION.tar.gz"
+tar czvf "$FILE" breakpad-$VERSION/
+mv "$FILE" ..
+popd
+echo "Archive created at $(pwd)/$FILE"
+
+exit 0
